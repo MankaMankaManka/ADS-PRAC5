51e9f2f1e56a1be0d0ab422fae08af7a1ed56863
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-04-11 00:57:05 +0930

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,32 +5,24 @@
 #include "RecursiveBinarySearch.h"
 
 int main(){
-std::vector<int> sampleOne= {1, 3, 5, 4, -5, 100, 7777, 2014};
-std::vector<int> sampleTwo= {0, 3, 5, 4, -5, 100, 7777, 2014};
-std::vector<int> sampleThree= {0, 3, 5, 4, -5, 100, 7777, 2014};
 QuickSort a;
 RecursiveBinarySearch b;
-BubbleSort c;
-c.bubbleSort(sampleThree);
+std::vector<int> numbers;
+int input;
+while (std::cin >> input){
+    numbers.push_back(input);
+}
 
-a.quickSort(sampleOne);
-if (b.search(sampleOne, 1)==1){
+a.quickSort(numbers);
+
+if (b.search(numbers, 1)==1){
     std::cout<<"true ";
 } else{
     std::cout<<"false ";
 }
-for (int i=0; i<sampleOne.size();i++){
-    std::cout<<sampleOne[i]<<" ";
+for (int i=0; i<numbers.size();i++){
+    std::cout<<numbers[i]<<" ";
     }
-    
-a.quickSort(sampleTwo);
-if (b.search(sampleTwo, 1)==1){
-    std::cout<<"true ";
-} else{
-    std::cout<<"false ";
-}
-for (int i=0; i<sampleTwo.size();i++){
-    std::cout<<sampleTwo[i]<<" ";
-    }
+
 }
 
