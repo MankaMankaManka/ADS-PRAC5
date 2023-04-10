d026ca6a4b1925a8f7ccfea5709e8c4845e0379e
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-04-10 23:47:09 +0930

    Update and rename bubblesort test to RecursiveBinarySearch.h

---
+++ RecursiveBinarySearch.h
@@ -0,0 +1,9 @@
+#ifndef RECURSIVEBINARYSEARCH_H
+#define RECURSIVEBINARYSEARCH_H
+class RecursiveBinarySearch
+{
+    public:
+        bool search(std::vector<int> list, int number);
+        bool doSearch(std::vector<int> list, int number, int start, int end);
+};
+#endif
