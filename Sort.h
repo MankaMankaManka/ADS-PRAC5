abb56d081d987d826a6cb26b28c687f0074297c3
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-04-11 00:33:43 +0930

    Add files via upload

--- Sort.h
+++ Sort.h
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
-
+#ifndef SORT_H
+#define SORT_H
 class Sort
 {
     public:
         std::vector<int> sort(std::vector<int> list);
 };
+#endif
--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@
 int main(){
 std::vector<int> sampleOne= {1, 3, 5, 4, -5, 100, 7777, 2014};
 std::vector<int> sampleTwo= {0, 3, 5, 4, -5, 100, 7777, 2014};
+std::vector<int> sampleThree= {0, 3, 5, 4, -5, 100, 7777, 2014};
 QuickSort a;
 RecursiveBinarySearch b;
+BubbleSort c;
+c.bubbleSort(sampleThree);
 
 a.quickSort(sampleOne);
 if (b.search(sampleOne, 1)==1){
